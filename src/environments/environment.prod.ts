9818c6c601c4319e4f2ff1b048c2fd0447993678
750752 <tsairampavan@gmail.com>
2023-04-15 15:35:00 +0530

    5th commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAgMyiun4Ms5KuO2b3loQMH2o1jgZ8viZ8",
+    authDomain: "ang-blog-db9a4.firebaseapp.com",
+    projectId: "ang-blog-db9a4",
+    storageBucket: "ang-blog-db9a4.appspot.com",
+    messagingSenderId: "397173814877",
+    appId: "1:397173814877:web:3a8e979f14010a222f7fb4"
+  }
 };
